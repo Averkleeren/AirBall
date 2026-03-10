6b4101b05c3b36b08ff8b833e3da6ca67621cb4b
amatys <amatys@rpi.edu>
2026-03-10 17:08:58 -0400

    added basic accordion component & utils

---
+++ client/components/ui/accordion.tsx
@@ -0,0 +1,27 @@
+'use client'
+
+import * as React from 'react'
+import * as AccordionPrimitive from '@radix-ui/react-accordion'
+
+import { cn } from '@/lib/utils'
+
+function Accordion({
+  ...props
+}: React.ComponentProps<typeof AccordionPrimitive.Root>) {
+  return <AccordionPrimitive.Root data-slot="accordion" {...props} />
+}
+
+function AccordionItem({
+  className,
+  ...props
+}: React.ComponentProps<typeof AccordionPrimitive.Item>) {
+  return (
+    <AccordionPrimitive.Item
+      data-slot="accordion-item"
+      className={cn('border-b last:border-b-0', className)}
+      {...props}
+    />
+  )
+}
+
+export { Accordion, AccordionItem }
