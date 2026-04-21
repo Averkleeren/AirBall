e36df6fba4bfcab88d6a000d11edabff16cee72c
amatys <amatys@rpi.edu>
2026-04-21 17:31:47 -0400

    added item media and media variants

--- client/components/ui/item.tsx
+++ client/components/ui/item.tsx
@@ -45,6 +45,38 @@ function Item({
   )
 }
 
+const itemMediaVariants = cva(
+  'flex shrink-0 items-center justify-center gap-2 group-has-[[data-slot=item-description]]/item:self-start [&_svg]:pointer-events-none group-has-[[data-slot=item-description]]/item:translate-y-0.5',
+  {
+    variants: {
+      variant: {
+        default: 'bg-transparent',
+        icon: "size-8 border rounded-sm bg-muted [&_svg:not([class*='size-'])]:size-4",
+        image:
+          'size-10 rounded-sm overflow-hidden [&_img]:size-full [&_img]:object-cover',
+      },
+    },
+    defaultVariants: {
+      variant: 'default',
+    },
+  },
+)
+
+function ItemMedia({
+  className,
+  variant = 'default',
+  ...props
+}: React.ComponentProps<'div'> & VariantProps<typeof itemMediaVariants>) {
+  return (
+    <div
+      data-slot="item-media"
+      data-variant={variant}
+      className={cn(itemMediaVariants({ variant, className }))}
+      {...props}
+    />
+  )
+}
+
 function ItemContent({ className, ...props }: React.ComponentProps<'div'>) {
   return (
     <div
@@ -60,5 +92,6 @@ function ItemContent({ className, ...props }: React.ComponentProps<'div'>) {
 
 export {
   Item,
+  ItemMedia,
   ItemContent,
 }
