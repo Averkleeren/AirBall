942b74edf5a6cff7f0b57cb61428c36f755da3bf
amatys <amatys@rpi.edu>
2026-04-28 12:08:03 -0400

    side bar input, header footer, seperator, content added

--- client/components/ui/sidebar.tsx
+++ client/components/ui/sidebar.tsx
@@ -6,6 +6,8 @@ import { cn } from '@/lib/utils'
 import { TooltipProvider } from '@/components/ui/tooltip'
 import { PanelLeftIcon } from 'lucide-react'
 import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Separator } from '@/components/ui/separator'
 
 import {
   Sheet,
@@ -260,4 +262,51 @@ function SidebarInset({ className, ...props }: React.ComponentProps<'main'>) {
   )
 }
 
-export { useSidebar, SidebarProvider, Sidebar, SidebarTrigger, SidebarRail, SidebarInset }
+function SidebarInput({ className, ...props }: React.ComponentProps<typeof Input>) {
+  return (
+    <Input
+      data-slot="sidebar-input"
+      data-sidebar="input"
+      className={cn('bg-background h-8 w-full shadow-none', className)}
+      {...props}
+    />
+  )
+}
+
+function SidebarHeader({ className, ...props }: React.ComponentProps<'div'>) {
+  return (
+    <div data-slot="sidebar-header" data-sidebar="header"
+      className={cn('flex flex-col gap-2 p-2', className)} {...props} />
+  )
+}
+
+function SidebarFooter({ className, ...props }: React.ComponentProps<'div'>) {
+  return (
+    <div data-slot="sidebar-footer" data-sidebar="footer"
+      className={cn('flex flex-col gap-2 p-2', className)} {...props} />
+  )
+}
+
+function SidebarSeparator({ className, ...props }: React.ComponentProps<typeof Separator>) {
+  return (
+    <Separator
+      data-slot="sidebar-separator"
+      data-sidebar="separator"
+      className={cn('bg-sidebar-border mx-2 w-auto', className)}
+      {...props}
+    />
+  )
+}
+
+function SidebarContent({ className, ...props }: React.ComponentProps<'div'>) {
+  return (
+    <div
+      data-slot="sidebar-content"
+      data-sidebar="content"
+      className={cn('flex min-h-0 flex-1 flex-col gap-2 overflow-auto group-data-[collapsible=icon]:overflow-hidden', className)}
+      {...props}
+    />
+  )
+}
+
+export { useSidebar, SidebarProvider, Sidebar, SidebarTrigger, SidebarRail, SidebarInset, SidebarInput, SidebarHeader, SidebarFooter, SidebarSeparator, SidebarContent }
